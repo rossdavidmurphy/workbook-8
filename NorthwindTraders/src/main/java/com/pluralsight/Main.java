e12c0afe37428bb358f1c4f319e1e4615a6b7310
Ross Murphy <rossdavidmurphy@gmail.com>
2025-06-10 10:24:21 -0400

    pass username and password as command line args

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -11,11 +11,21 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // load the MySQL Driver, next line no longer needed in newer versions of Java
 //         Class.forName("com.mysql.cj.jdbc.Driver");
+        if (args.length != 2) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.UsingDriverManager <username> <password>");
+            System.exit(1);
+        }
+
+        String username = args[0];
+        String password = args[1];
+
 
         // 1. open a connection to the database
         // use the database URL to point to the correct database
         Connection connection;
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "FakePassword");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
         // create statement
         // the statement is tied to the open connection
@@ -29,8 +39,10 @@ public class Main {
 
         // process the results
         while (results.next()) {
-            String city = results.getString("ProductName");
-            System.out.println(city);
+            String productName = results.getString("ProductName");
+//            String productId = results.getNString("ProductID");
+//            System.out.println(productInfo);
+            System.out.println(productName);
         }
 
         // 3. Close resources
